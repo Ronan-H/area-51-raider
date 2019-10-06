aceaf45640fb58220561398da7f9d9d4d8b7b5dc
Ronan-H <g00342393@gmit.ie>
2019-10-06 09:59:13 +0100

    Added player and player movement

---
+++ Area 51 Raider/Assets/__Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float horizMag = Input.GetAxis("Horizontal");
+        float verticalMag = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(horizMag, verticalMag);
+        rb2d.AddForce(movement * speed);
+    }
+}
