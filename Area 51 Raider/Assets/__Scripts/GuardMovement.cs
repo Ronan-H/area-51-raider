d567d9c0d3b33295c58d9446c613d6f1aaeadbad
Ronan-H <g00342393@gmit.ie>
2019-10-06 11:51:53 +0100

    Added guard movement using waypoints

---
+++ Area 51 Raider/Assets/__Scripts/GuardMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private GameObject[] waypointObjects;
+
+    private Rigidbody2D rb2d;
+    private Transform[] waypoints;
+    private int waypointIndex;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        waypoints = new Transform[waypointObjects.Length];
+        for (int i = 0; i < waypoints.Length; i++)
+        {
+            waypoints[i] = waypointObjects[i].transform;
+        }
+    }
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[waypointIndex].position, speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, waypoints[waypointIndex].position) < 0.1f)
+        {
+            waypointIndex = (waypointIndex + 1) % waypoints.Length;
+        }
+    }
+}
