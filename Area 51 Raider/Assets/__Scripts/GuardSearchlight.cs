30c58c9511b54a9fd2f356a21401b461d3c3c0ec
Ronan-H <g00342393@gmit.ie>
2019-10-07 16:45:32 +0100

    Added searchlight cone for the guards

---
+++ Area 51 Raider/Assets/__Scripts/GuardSearchlight.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardSearchlight : MonoBehaviour
+{
+    [SerializeField]
+    private float fov;
+
+    [SerializeField]
+    private float searchDist;
+
+    private int segments;
+
+    private LineRenderer lineRenderer;
+
+    void Start()
+    {
+        lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+        lineRenderer.widthMultiplier = 0.03f;
+        lineRenderer.positionCount = segments;
+        lineRenderer.startColor = Color.yellow;
+        lineRenderer.endColor = Color.yellow;
+        lineRenderer.loop = true;
+    }
+    
+    void Update()
+    {
+        segments = (int)Mathf.Max(Mathf.Ceil(fov / 10), 3);
+        DrawSearchlight();
+    }
+
+    void DrawSearchlight()
+    {
+        List<Vector2> arcPoints = new List<Vector2>();
+        float angle = transform.rotation.eulerAngles.z - (fov / 2);
+        arcPoints.Add(transform.position);
+        for (int i = 0; i <= segments; i++)
+        {
+            float x = transform.position.x + Mathf.Sin(Mathf.Deg2Rad * angle) * searchDist;
+            float y = transform.position.y + Mathf.Cos(Mathf.Deg2Rad * angle) * searchDist;
+
+            arcPoints.Add(new Vector2(x, y));
+
+            angle += (fov / segments);
+        }
+
+        lineRenderer.positionCount = arcPoints.Count;
+        for (int i = 0; i < arcPoints.Count; i++) {
+            lineRenderer.SetPosition(i, arcPoints[i]);
+        }
+    }
+}
